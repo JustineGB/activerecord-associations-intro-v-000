c4faf2eb815adff9655c353724f49c1afabfbc6c
Justine Bricknell <justinegayton@gmail.com>
2018-10-09 18:22:22 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
-  class CreateArtists < ActiveRecord::Migration[4.2]
-    def change
-      create_table :artists do |t|
-        t.string :name
-      end
+  def change
+    create_table :artists do |t|
+      t.string :name
     end
   end
+end
 end
